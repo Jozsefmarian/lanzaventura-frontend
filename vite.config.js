a79e542e1900ba12ef2ff6e1cd9132a45a9b4a37
Jozsefmarian <jozsefmarian@yahoo.com>
2025-04-28 20:53:29 +0200

    Disable minification and enable development mode

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,12 @@ export default defineConfig({
   root: './', // Alapértelmezett könyvtár
   build: {
     outDir: 'dist', // Az exportált fájlok mappája
+    minify: false,  // A minifikálás letiltása
   },
   resolve: {
     alias: {
       '@': '/src', // Alias a src mappára
     },
   },
+  mode: 'development', // Fejlesztési mód engedélyezése
 });
