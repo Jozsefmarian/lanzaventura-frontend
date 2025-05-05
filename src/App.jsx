5eac2a0fa90d677ac90cddd942dd9971b647c34c
Jozsefmarian <jozsefmarian@yahoo.com>
2025-05-05 18:44:18 +0200

    Fix: háttérkép megjelenítése és layout javítása

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import SearchForm from "./components/SearchForm";
 import HotelList from "./components/HotelList";
+import "./styles/SearchForm.css";
 
 function App() {
   const [results, setResults] = useState([]);
 
   return (
-    <div className="app-container">
-      <SearchForm setResults={setResults} />
-      <HotelList hotels={results} />
+    <div className="search-background">
+      <div className="app-container">
+        <SearchForm setResults={setResults} />
+        <HotelList hotels={results} />
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
